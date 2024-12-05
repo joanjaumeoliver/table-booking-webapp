d1c17b8ab5758a8fb4dd8ccd67ce20a1e9bcc6b4
joanjaumeoliver <jjoliver@ams-h2o.com>
2024-12-05 16:20:35 +0100

    Add Flexbox to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const heroStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  padding: '0rem 2rem'
+};
+
+const heroTextStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+};
 
 function Hero() {
   return (
-    <section id="hero">
-      <img src="hero-image.jpg" alt="Little Lemon Restaurant" />
-      <h2>Welcome to Little Lemon</h2>
-      <p>Your favorite restaurant for delicious food!</p>
-    </section>
+    <section style={heroStyle}>
+    <div style={heroTextStyle}>
+      <h2>Little Lemon</h2>
+      <h3>Chicago</h3>
+      <p>
+        We are a family owned Mediterraneran restuarent, focusd on
+        traditional recipes served with modern twist.
+      </p>
+      <Link to="/reservation">
+        <button aria-label="on Click ">Reserve Table</button>
+      </Link>
+    </div>
+    <img src='banner.jpg' alt="banner-image" />
+  </section>
   );
 }
 
