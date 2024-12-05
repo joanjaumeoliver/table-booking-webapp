67ae7de4dc688b8814d07f939f9989df96d306de
joanjaumeoliver <jjoliver@ams-h2o.com>
2024-12-05 16:20:44 +0100

    Corrected Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Footer() {
+const Footer = () => {
   return (
-  <footer id="footer-navigation">
-    <div class="logo">
-      <img src="logo.png" alt="Little Lemon Logo" />
-    </div>
-    <nav>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#menu">Menu</a></li>
-        <li><a href="#reservations">Reservations</a></li>
-        <li><a href="#order">Order Online</a></li>
-        <li><a href="#login">Login</a></li>
-      </ul>
-    </nav>
-    <div class="contact-details">
-      <p>Contact us at: <a href="mailto:info@littlelemon.com">info@littlelemon.com</a></p>
-    </div>
-    <div class="social-media">
-      <a href="https://facebook.com/">Facebook</a>
-      <a href="https://instagram.com/">Instagram</a>
-      <a href="https://twitter.com/">Twitter</a>
-    </div>
-  </footer>
+    <section>
+        <>
+          <img src='logo.png' alt="company-image" />
+          <p>
+            We are a family owned Mediterraneran restuarant, focused on
+            traditional recipes servred with a modern twist{" "}
+          </p>
+        </>
+        <>
+          <h3>Important Links</h3>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/coming-soon">About</Link>
+            </li>
+            <li>
+              <Link to="/reservations">Reservations</Link>
+            </li>
+            <li>
+              <Link to="/coming-soon">Order Online</Link>
+            </li>
+            <li>
+              <Link to="/coming-soon">Login</Link>
+            </li>
+          </ul>
+        </>
+
+        <>
+          <h3>Contact us</h3>
+          <ul>
+            <li>
+              Address : <br /> Towncity USA
+            </li>
+            <li>
+              Phone : <br /> +000000000
+            </li>
+            <li>
+              Email : <br /> abc@gmail.com
+            </li>
+          </ul>
+        </>
+        <>
+          <h3>Social Links</h3>
+          <ul>
+            <li>
+              <a href="/">Facebook</a>
+            </li>
+            <li>
+              <a href="/">Instagram</a>
+            </li>
+            <li>
+              <a href="/">Youtube</a>
+            </li>
+          </ul>
+        </>
+    </section>
   );
 }
 
