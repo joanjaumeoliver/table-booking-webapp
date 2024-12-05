5f37c0473d44729c817974fa7ce900186d48a3d4
joanjaumeoliver <jjoliver@ams-h2o.com>
2024-12-05 11:37:18 +0100

    Set up semantic HTML

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="logo.png" alt="Little Lemon Logo" />
+      <h1>Little Lemon</h1>
+      <p>Welcome to Little Lemon, the best restaurant in town!</p>
+    </header>
+  );
+}
+
+export default Header;
