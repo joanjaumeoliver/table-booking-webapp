1d46d9b6e0d843e5c14a4b623379019de28c39fe
joanjaumeoliver <jjoliver@ams-h2o.com>
2024-12-12 17:03:47 +0100

    Improved comming soon

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 const ComingSoon = () => {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '50vh', // Full height of the viewport
+    textAlign: 'center',
+    padding: '20px',
+  };
+
+  const headingStyle = {
+    fontSize: '8vw', // Responsive font size based on the viewport width
+    marginBottom: '20px',
+  };
+
+  const paragraphStyle = {
+    fontSize: '3vw', // Responsive font size for paragraph
+    maxWidth: '600px', // Optional max width for better readability
+    margin: '0 auto',
+  };
+
   return (
-    <div>
-      <h1>Coming Soon!</h1>
-      <p>This page is under development. Stay tuned!</p>
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>Coming Soon!</h1>
+      <p style={paragraphStyle}>This page is under development. Stay tuned!</p>
     </div>
   );
 };
